cef2d28cdec9ddf6d37577763d1f36eb970075db
Manh <manhdl.ptit@gmail.com>
2026-07-30 11:15:17 +0700

    feat: password not match info profile user and password not match 3 the last

--- app/python/change_password.py
+++ app/python/change_password.py
@@ -20,10 +20,48 @@ def change_password_when_logged_in():
     find_user = User.query.filter(User.id == int(id)).first()
 
     if not check_password_hash(find_user.password, current_password):
-        return jsonify({"Error" : "Password current not correct"})
+        return jsonify({"Error" : "Password current not correct"}), 400
 
+    password_refresh = (str(new_password).lower()).replace(" ", "")
+
+    create_fullName_refresh = (str(find_user.full_name).lower()).replace(" ", "")
+    if password_refresh == create_fullName_refresh:
+        return jsonify({"Error" : "Password not match profile data"}), 400
+    
+    create_email_refresh = (str(find_user.email).lower()).replace(" ", "")
+    if password_refresh == create_email_refresh:
+          return jsonify({"Error" : "Password not match profile data"}), 400
+    
+    create_phoneNumber_refresh = (str(find_user.phone_number).lower()).replace(" ", "")
+    if password_refresh == create_phoneNumber_refresh:
+        return jsonify({"Error" : "Password not match profile data"}), 400
+    
+    create_address_refresh = (str(find_user.address).lower()).replace(" ", "")
+    if password_refresh == create_address_refresh:
+        return jsonify({"Error" : "Password not match profile data"}), 400
+
+    create_city_refresh = (str(find_user.city).lower()).replace(" ", "")
+    if password_refresh == create_city_refresh:
+        return jsonify({"Error" : "Password not match profile data"}), 400
+
+    create_country_refresh = (str(find_user.country).lower()).replace(" ", "")
+    if password_refresh == create_country_refresh:
+        return jsonify({"Error" : "Password not match profile data"}), 400
+
+    check_datetime_password = Password.query.with_entities(Password.password_used).order_by(Password.date.desc()).limit(3)
+    list_password = []
+    for per_password in check_datetime_password:
+        for _ in per_password:
+            list_password.append(per_password[0])
+    for check_password in list_password:
+        if check_password_hash(check_password, new_password):
+            return jsonify({"error" : "the password must not match the last three passwords."}), 400
+    
+    
     if len(new_password) < 8 or len(new_password) > 32:
         return jsonify({"Error" : "Password between 8 and 32 characters"}), 400
+
+    
     
     password = generate_password_hash(new_password)
     find_user.password = password
